1295a32eb6596ffe51f5ebe023f13826d29a0ac3
Juan Carlos Valdés <idevkingos@gmail.com>
2018-10-22 20:53:52 -0300

    Configuro Express y middlewares

--- container.js
+++ container.js
@@ -4,19 +4,19 @@ const path = require('path');
 const container = dependable.container();
 
 const simpleDependencies = [
-    ['_', 'lodash']
+  ['_', 'lodash']
 ];
-simpleDependencies.forEach(function(val) {
-    container.register(val[0], function() {
-        return require(val[1]);
-    });
+simpleDependencies.forEach((val) => {
+  container.register(val[0], function() {
+    return require(val[1]);
+  });
 });
 
 container.load(path.join(__dirname, 'controllers'));
 container.load(path.join(__dirname, 'helpers'));
 
 container.register('container', function() {
-    return container;
+  return container;
 });
 
 module.exports = container;
--- controllers/users.js
+++ controllers/users.js
@@ -4,9 +4,14 @@ module.exports = function(_) {
   return {
     SetRouting: function(router) {
       router.get('/', this.indexPage);
+      router.get('/signup', this.getSignUp);
+
     },
     indexPage: function(req, res) {
       return res.render('index', {})
+    },
+    getSignUp: function(req, res) {
+      return res.render('signup', {})
     }
   }
 }
